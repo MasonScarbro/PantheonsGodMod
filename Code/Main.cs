a09c4420af0874a3b1508e14bbf06050c2ab167e
MasonScarbro <masonscarbropf@gmail.com>
2023-07-18 23:10:22 -0400

    Vers 0.3.0, added god spec traits

--- Code/Main.cs
+++ Code/Main.cs
@@ -1,6 +1,6 @@
 /* 
 AUTHOR: MASON SCARBRO
-VERSION: 0.2.0
+VERSION: 0.3.0
 */
 using System;
 using NCMS;
--- Code/Traits.cs
+++ Code/Traits.cs
@@ -1,6 +1,6 @@
 /* 
 AUTHOR: MASON SCARBRO
-VERSION: 0.2.0
+VERSION: 0.3.0
 */
 using System;
 using System.Threading;
@@ -30,6 +30,12 @@ namespace GodsAndPantheons
             - Add a special power to stars God
             */
 
+            /*some notes pTarget typically reps the target that the actor is currently facing pSelf is the Actors self
+            a typically stands for the actor a is a assembly reference to the actor however we can also declare an Actor object with a
+            you will notice that s typically used in the spawning of things or usage of effects, as of now I cant find what reflection actually does or why its used
+            from what I can tell this is typically used when using attacks that might want to be used for range to 
+            locate the target and kill him, TESTED. I deleted the line and just used the base game a call it didnt seem to efffect anything? */
+
             
             ActorTrait chaosGod = new ActorTrait();
             chaosGod.id = "God Of Chaos";
@@ -56,13 +62,16 @@ namespace GodsAndPantheons
             sunGod.base_stats[S.speed] += 80f;
             sunGod.base_stats[S.dodge] += 30f;
             sunGod.base_stats[S.accuracy] += 10f;
+            sunGod.base_stats[S.range] += 100f;
             sunGod.action_attack_target = new AttackAction(ActionLibrary.addBurningEffectOnTarget);
             sunGod.action_attack_target = new AttackAction(ActionLibrary.addSlowEffectOnTarget);
             sunGod.action_attack_target = new AttackAction(sunGodAttack);
             sunGod.action_death = (WorldAction)Delegate.Combine(sunGod.action_death, new WorldAction(sunGodsDeath));
             AssetManager.traits.add(sunGod);
             PlayerConfig.unlockTrait(sunGod.id);
+            sunGod.action_special_effect = (WorldAction)Delegate.Combine(sunGod.action_special_effect, new WorldAction(sunGodAutoTrait));
             addTraitToLocalizedLibrary(sunGod.id, "Tis' The God Of light!");
+            
 
             ActorTrait darkGod = new ActorTrait();
             darkGod.id = "God Of the Night";
@@ -77,6 +86,7 @@ namespace GodsAndPantheons
             darkGod.action_death = (WorldAction)Delegate.Combine(darkGod.action_death, new WorldAction(darkGodsDeath));
             AssetManager.traits.add(darkGod);
             PlayerConfig.unlockTrait(darkGod.id);
+            darkGod.action_special_effect = (WorldAction)Delegate.Combine(darkGod.action_special_effect, new WorldAction(darkGodAutoTrait));
             addTraitToLocalizedLibrary(darkGod.id, "Tis' The God Of darkness!");
 
             ActorTrait knowledgeGod = new ActorTrait();
@@ -93,6 +103,7 @@ namespace GodsAndPantheons
             knowledgeGod.action_attack_target = new AttackAction(knowledgeGodAttack);
             AssetManager.traits.add(knowledgeGod);
             PlayerConfig.unlockTrait(knowledgeGod.id);
+            knowledgeGod.action_special_effect = (WorldAction)Delegate.Combine(knowledgeGod.action_special_effect, new WorldAction(knowledgeGodAutoTrait));
             addTraitToLocalizedLibrary(knowledgeGod.id, "Tis' The God Of Knowledges!");
 
             ActorTrait starsGod = new ActorTrait();
@@ -108,10 +119,11 @@ namespace GodsAndPantheons
             starsGod.action_attack_target = new AttackAction(ActionLibrary.addFrozenEffectOnTarget);
             AssetManager.traits.add(starsGod);
             PlayerConfig.unlockTrait(starsGod.id);
+            starsGod.action_special_effect = (WorldAction)Delegate.Combine(starsGod.action_special_effect, new WorldAction(starsGodAutoTrait));
             addTraitToLocalizedLibrary(starsGod.id, "Now Cometh the Age of stars, A Thousand Year Voyage under the wisdom of the moon");
 
             ActorTrait earthGod = new ActorTrait();
-            earthGod.id = "God Of the the Earth";
+            earthGod.id = "God Of the Earth";
             earthGod.path_icon = "ui/icons/achievements/achievements_thedemon";
             earthGod.base_stats[S.damage] += 40f;
             earthGod.base_stats[S.health] += 1000;
@@ -122,10 +134,11 @@ namespace GodsAndPantheons
             earthGod.base_stats[S.intelligence] += 3f;
             AssetManager.traits.add(earthGod);
             PlayerConfig.unlockTrait(earthGod.id);
+            earthGod.action_special_effect = (WorldAction)Delegate.Combine(earthGod.action_special_effect, new WorldAction(earthGodAutoTrait));
             addTraitToLocalizedLibrary(earthGod.id, "God of the Natural Enviornment, The titan of creation");
 
             ActorTrait subGod = new ActorTrait();
-            subGod.id = "God Of You Decide";
+            subGod.id = "LesserGod";
             subGod.path_icon = "ui/icons/achievements/achievements_thedemon";
             subGod.base_stats[S.damage] += 5f;
             subGod.base_stats[S.health] += 400;
@@ -154,7 +167,7 @@ namespace GodsAndPantheons
         public static bool sunGodsDeath(BaseSimObject pTarget, WorldTile pTile = null)
         {
         
-            Actor pActor = (Actor)pTarget;
+
             World.world.eraManager.setEra(S.age_dark, true);
 
 
@@ -165,7 +178,6 @@ namespace GodsAndPantheons
         public static bool darkGodsDeath(BaseSimObject pTarget, WorldTile pTile = null)
         {
         
-            Actor pActor = (Actor)pTarget;
             World.world.eraManager.setEra(S.age_sun, true);
 
 
@@ -186,7 +198,7 @@ namespace GodsAndPantheons
                     // randomly spawns a flash of fire or acid on the tile 
                     MapBox.instance.dropManager.spawn(pTile, "fire", 5f, -1f);
                     MapBox.instance.dropManager.spawn(pTile, "acid", 5f, -1f);
-                    MapBox.instance.dropManager.spawn(pTile, "fire", 5f, -1f);
+                    MapBox.instance.dropManager.spawn(pTile, "fire", 5f, -1f); // Drops fire from distance 5 with scale of one at current tile
                 }
                 if (Toolbox.randomChance(0.01f))
                 {
@@ -208,7 +220,7 @@ namespace GodsAndPantheons
                 {
                     ActionLibrary.castLightning(null, pTarget, null); // Casts Lightning on the target
                 }
-                if (Toolbox.randomChance(0.5f))
+                if (Toolbox.randomChance(0.05f))
                 {
                     EffectsLibrary.spawn("fx_meteorite", pTarget.currentTile, "meteorite_disaster", null, 0f, -1f, -1f);    //spawn 1 meteorite
                     pSelf.a.addStatusEffect("invincible", 5f);
@@ -233,7 +245,7 @@ namespace GodsAndPantheons
                 Actor a = Reflection.GetField(pTarget.GetType(), pTarget, "a") as Actor;
 
                 
-                if (Toolbox.randomChance(0.1f))
+                if (Toolbox.randomChance(0.05f))
                 {
                     EffectsLibrary.spawn("fx_antimatter_effect", pTarget.a.currentTile, null, null, 0f, -1f, -1f);
                     pSelf.a.addStatusEffect("invincible", 5f);
@@ -244,20 +256,22 @@ namespace GodsAndPantheons
             return false;
         }
 
+        
+
         public static bool sunGodAttack(BaseSimObject pSelf, BaseSimObject pTarget, WorldTile pTile)
         {
 
             if (pTarget != null)
             {
-                Actor a = Reflection.GetField(pTarget.GetType(), pTarget, "a") as Actor;
-
                 
-                if (Toolbox.randomChance(0.5f))
+                Actor a = Reflection.GetField(pTarget.GetType(), pTarget, "a") as Actor;
+                
+                if (Toolbox.randomChance(0.08f))
                 {
                     EffectsLibrary.spawn("fx_napalm_flash", pTarget.a.currentTile, null, null, 0f, -1f, -1f);
                     pSelf.a.addStatusEffect("invincible", 5f);
                 }
-                if (Toolbox.randomChance(0.1f))
+                if (Toolbox.randomChance(0.5f))
                 {
                     EffectsLibrary.spawn("fx_slash", pTarget.a.currentTile, null, null, 0f, -1f, -1f);
                 }
@@ -297,7 +311,127 @@ namespace GodsAndPantheons
 
 
          
+        public static bool sunGodAutoTrait(BaseSimObject pTarget, WorldTile pTile)
+        {
+            
+            if (pTarget.a != null)
+            {
+                if (pTarget.a.hasTrait("God Of light"))
+                {
+                    pTarget.a.addTrait("blessed");
+                    pTarget.a.addTrait("shiny");
+                    pTarget.a.addTrait("agile");
+                    pTarget.a.addTrait("fire_proof");
+                    pTarget.a.addTrait("fire_blood");
+                    pTarget.a.addTrait("weightless");
+                    pTarget.a.addTrait("fast");
+                    pTarget.a.addTrait("energized");
+                    pTarget.a.addTrait("light_lamp");
+                    pTarget.a.addTrait("immortal");
 
+                }
+                
+
+            }
+            return true;
+        }
+
+        public static bool knowledgeGodAutoTrait(BaseSimObject pTarget, WorldTile pTile)
+        {
+            Actor a = Reflection.GetField(pTarget.GetType(), pTarget, "a") as Actor;
+            if (pTarget.a != null)
+            {
+                if (pTarget.a.hasTrait("God Of Knowledge"))
+                {
+                    pTarget.a.addTrait("blessed");
+                    pTarget.a.addTrait("genius");
+                    pTarget.a.addTrait("frost_proof");
+                    pTarget.a.addTrait("fire_proof");
+                    pTarget.a.addTrait("fire_blood");
+                    pTarget.a.addTrait("tough");
+                    pTarget.a.addTrait("strong_minded");
+                    pTarget.a.addTrait("energized");
+                    pTarget.a.addTrait("immortal");
+                    pTarget.a.addTrait("wise");
+
+
+                }
+                
+
+            }
+            return true;
+        }
+
+        public static bool darkGodAutoTrait(BaseSimObject pTarget, WorldTile pTile)
+        {
+            Actor a = Reflection.GetField(pTarget.GetType(), pTarget, "a") as Actor;
+            if (pTarget.a != null)
+            {
+                if (pTarget.a.hasTrait("God Of the Night"))
+                {
+                    pTarget.a.addTrait("blessed");
+                    pTarget.a.addTrait("bloodlust");
+                    pTarget.a.addTrait("agile");
+                    pTarget.a.addTrait("frost_proof");
+                    pTarget.a.addTrait("weightless");
+                    pTarget.a.addTrait("cold_aura");
+                    pTarget.a.addTrait("energized");
+                    pTarget.a.addTrait("immortal");
+                    pTarget.a.addTrait("nightchild");
+                    pTarget.a.addTrait("moonchild");
+
+                }
+                
+
+            }
+            return true;
+        }
+
+        public static bool starsGodAutoTrait(BaseSimObject pTarget, WorldTile pTile)
+        {
+            Actor a = Reflection.GetField(pTarget.GetType(), pTarget, "a") as Actor;
+            if (pTarget.a != null)
+            {
+                if (pTarget.a.hasTrait("God Of the Stars"))
+                {
+                    pTarget.a.addTrait("blessed");
+                    pTarget.a.addTrait("agile");
+                    pTarget.a.addTrait("frost_proof");
+                    pTarget.a.addTrait("weightless");
+                    pTarget.a.addTrait("shiny");
+                    pTarget.a.addTrait("energized");
+                    pTarget.a.addTrait("immortal");
+                    pTarget.a.addTrait("nightchild");
+                    pTarget.a.addTrait("moonchild");
+
+                }
+                
+
+            }
+            return true;
+        }
+
+        public static bool earthGodAutoTrait(BaseSimObject pTarget, WorldTile pTile)
+        {
+
+            if (pTarget.a != null)
+            {
+                if (pTarget.a.hasTrait("God Of the Earth"))
+                {
+                    pTarget.a.addTrait("blessed");
+                    pTarget.a.addTrait("giant");
+                    pTarget.a.addTrait("strong");
+                    pTarget.a.addTrait("frost_proof");
+                    pTarget.a.addTrait("fat");
+                    pTarget.a.addTrait("tough");
+                    pTarget.a.addTrait("immortal");
+
+                }
+                
+
+            }
+            return true;
+        }
 
          public static void addTraitToLocalizedLibrary(string id, string description)        
          {
