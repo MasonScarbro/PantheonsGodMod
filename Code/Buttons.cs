c3423884f32fec3642a1b33740b19cb3d2e70cb0
MelvinShwuaner <83349478+MelvinShwuaner@users.noreply.github.com>
2024-07-09 12:18:40 +0300

    Update Buttons.cs

--- Code/Buttons.cs
+++ Code/Buttons.cs
@@ -154,10 +154,8 @@ namespace GodsAndPantheons
         public static void ToggleEra(){
             if(Main.savedSettings.deathera){
                 Main.savedSettings.deathera = false;
-                Traits.deathera = false;
             }else{
                 Main.savedSettings.deathera = true;
-                Traits.deathera = true;
             }
         }
     }
