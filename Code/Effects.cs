331114b818f62bb60d425deb282b43291daca436
MelvinShwuaner <83349478+MelvinShwuaner@users.noreply.github.com>
2024-08-18 16:22:04 +0300

    fixed bug

--- Code/Effects.cs
+++ Code/Effects.cs
@@ -244,10 +244,17 @@ namespace GodsAndPantheons
         public static bool InvisibleEffect(BaseSimObject pTarget, WorldTile pTile)
         {
             Color mycolor = pTarget.GetComponent<SpriteRenderer>().color;
-            if(mycolor.a != 0.4)
+            if (pTarget.activeStatus_dict["Invisible"]._end_time > World.world.getCurWorldTime())
             {
-                pTarget.a.restoreHealth(pTarget.a.getMaxHealth());
-                pTarget.GetComponent<SpriteRenderer>().color = new Color(mycolor.r, mycolor.g, mycolor.b, 0.4f);
+                if (mycolor.a != 0.4)
+                {
+                    pTarget.a.restoreHealth(pTarget.a.getMaxHealth());
+                    pTarget.GetComponent<SpriteRenderer>().color = new Color(mycolor.r, mycolor.g, mycolor.b, 0.4f);
+                }
+            }
+            else
+            {
+                pTarget.GetComponent<SpriteRenderer>().color = new Color(mycolor.r, mycolor.g, mycolor.b, 1);
             }
             return true;
         }
